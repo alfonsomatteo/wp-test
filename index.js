9bcd319809084db692683d6a33a3b1f2d27b29df
alfonsomatteo <mail.alfonsomatteo@gmail.com>
2018-04-30 19:30:18 +0200

    add API and style changes

--- index.js
+++ index.js
@@ -1,46 +1,83 @@
+let $ = jQuery
+$(document).ready(function() {
 
-/*
+	var getTitle = function(){
+		
+    var film = $('#term').val();
+		var film = encodeURIComponent(film);
+		
+    if (film == '') {
+      $('#poster').removeClass('hide').addClass("fadeIn");  
+      $('#poster').html("<h2>You forget to enter a film title.</h2>");
+    } else {
+      
+      $('#poster').html("<h2>Searching now!</h2>");
 
+      $('#poster').removeClass('hide').addClass("fadeIn");
+      
+      $.getJSON("https://api.themoviedb.org/3/search/movie?"
+								+ '&query=' + film
+								+ '&include_video=true'
+								+ '&include_adult=false'
+								+ '&sort_by=popularity.desc'
+								+ '&language=en-US'
+								+ '&api_key=855d0f27ac90850576cebfa7bc46b9f6',
+							 			
+										//what to do with the data on success
+										function(data){
 
-*/
+													if(data.results !== 0) {
 
+                            $('#suggestedTitles').fadeIn(200);  
+														console.log("You are looking for " + film);
+														console.log(data);
 
-jQuery(function() {
-  jQuery.getJSON(
-  "https://api.themoviedb.org/3/search/person?api_key=855d0f27ac90850576cebfa7bc46b9f6&query=Colin%20Farrell",
-  function(actors) {
-  console.log(actors.results[0].id)
- 
-   for(let i = 0; i < actors.results[0].id; i++){
-    document.getElementById("place").innerHTML = "<li>" + actors.results[0].id + "</li>"
-   
+                            // first let's show the top results
+                            
+                            $.each(data.results,function(index, val){
+                                   $('#suggestedTitles').append('<li><p class="filmTitle">' + val.title + '&nbsp;<em style="color: #aeaeae; font-size: 0.6rem;">' + val.release_date.substring(0,4) + '</em></p><p class="filmId" style="display: none;">' + val.id + '</p></li>');
+                            });                               
+                            
+                            $('#suggestedTitles li').click(function(){
+                              
+                              $('#suggestedTitles').empty();
+                              var pickedFilmId = $(this).children('.filmId').text();
+                              var pickedFilm = $(this).children('.filmTitle').text();
+                              console.log('You picked ' + pickedFilm + ' with ID ' + pickedFilmId);                      
+                              
+                              $('#suggestedTitles').fadeOut(200);
+                              
+                              
+                              for(var i = 0; i < data.results.length; i++)
+                              {
+                                
+                                if(data.results[i].id == pickedFilmId)
+                                {
+                                    $('#poster').html('<div class="resultsHead"><img src="https://image.tmdb.org/t/p/w370_and_h556_bestv2/' + data.results[i].poster_path + '"/><h2>' + data.results[i].title + '</h2>');
+                                    $('#poster').append('<em class="releaseDate">' + data.results[i].release_date.substring(0,4) + '</em>' + '<br /><p class="description">' + data.results[i].overview + '</p></div><div class="clearfix"></div>');
+                                  
+                                    if(data.results[i].vote_average >= 7) {
+                                        $('#poster').append('<p>Your movie taste is: <strong>Pretentious Jerkface</strong></p>');
+                                    } else {
+                                        $('#poster').append('<p>Your movie taste is: <strong>Slobbering dingus</strong></p>');
+                                    }
+                                }
+                              }
+
+                            })
+														
+
+														
+
+												} else {
+													$('#poster').html("<h2>So sorry, we couldn't find anything.</h2>");
+												}									
+									}
+							 );
+    
+    } return false;
   }
-  
-  
-  ;
 
+$('#search').click(getTitle);
 
-
-  jQuery.getJSON(
-  "https://api.themoviedb.org/3/discover/movie?api_key=855d0f27ac90850576cebfa7bc46b9f6&with_cast=" + actors.results[0].id + "&sort_by=release_date:desc",
-  function(movies) {
-  console.log(movies)
-  
-  for(let i = 0; i < movies; i++){
-    document.getElementById("place").innerHTML = "<li>" + movies + "</li>"
-   
-  }
-
-  
-  
-  ;
-  }
-  );
-  }
-  );
-  });
-  
-  
-
- 
- 
+});
--- template-actor.php
+++ template-actor.php
@@ -26,13 +26,22 @@ wp_enqueue_script( 'index.js', get_stylesheet_directory_uri() . '/index.js', arr
 
 get_header(); ?>
 <?php $layout_class = shapely_get_layout_class(); ?>
-	<div class="row">
-		<div class="col-sm-12">
-			<div id="here">
-				<ul id="place"></ul>
 
+		
+		
+		
+			<div id="filmFinder">
+					<input type="text" id="term" class="textInput" placeholder="Find that film!"></input>
+					<ul id="suggestedTitles"></ul>
+					<input type="submit" id="search"></input>
+
+					<div id="poster" class="hide"></div>
 			</div>
-		</div>
-	</div>
+		
+			<div class="empty-div">
+				
+			</div>
+		
+
 <?php
 get_footer();
